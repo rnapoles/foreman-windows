9c1af56f17171ca4fab93702f6351b8a6e9eb7f5
Reinier Nápoles Martínez <rnapoles86@gmail.com>
2023-07-17 12:52:51 -0400

    Refactor project

---
+++ Foreman/Domain/ProcfileEntry.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Foreman
+{
+    class ProcfileEntry
+    {
+        private Procfile m_objProcfile = null;
+
+        private int m_port = 0;
+
+        private int m_intIndex = 0;
+
+        private string m_strName = null;
+
+        private string m_strCommand = null;
+
+        private Process m_objProcess = null;
+
+        public event Procfile.TextReceivedHandler TextReceived;
+
+        private Dictionary<string, string> m_envVariables = new();
+
+        public bool Active { private set; get; }
+
+        public string Name
+        {
+            get
+            {
+                return m_strName;
+            }
+        }
+
+        public ProcfileEntry(Procfile objProcfile, int intIndex, string strName, string strCommand, Dictionary<string, string> envVariables)
+        {
+            m_objProcfile = objProcfile;
+            m_intIndex = intIndex;
+            m_strName = strName;
+            m_strCommand = strCommand;
+
+            if (!envVariables.ContainsKey("PORT"))
+            {
+                m_strCommand = m_strCommand.Replace("$PORT", Port().ToString());
+            } else
+            {
+                int port = 0;
+                bool result = int.TryParse(envVariables["PORT"], out port);
+
+                if (result)
+                {
+                    port = Port(port);
+                    m_strCommand = m_strCommand.Replace("$PORT", port.ToString());
+                }
+            }
+
+            if (envVariables.Count > 0)
+            {
+                m_strCommand = ReplaceVariables(envVariables, m_strCommand);
+            }
+
+            
+            m_envVariables = envVariables;
+        }
+
+        public string ReplaceVariables(Dictionary<string, string> dictionary, string text)
+        {
+            foreach (var kvp in dictionary)
+            {
+                text = text.Replace($"${kvp.Key}", kvp.Value);
+            }
+            return text;
+        }
+
+        public string Header()
+        {
+            var time = DateTime.Now.ToString("HH:mm:ss");
+            return (String.Format(@"{{\cf{0} {1}  {2,-" + m_objProcfile.LongestNameLength() + "} |}} ", m_intIndex, time, m_strName));
+        }
+
+        public int Port(int port = 5000)
+        {
+
+            port = (port + (100 * (m_intIndex - 1)));
+            port = PortFinderService.GetPort(port);
+            this.m_port = port;
+
+            return port;
+        }
+
+        public void Start()
+        {
+
+            string[] parts = m_strCommand.Split(' ');
+            string fileName = parts.First();
+            string arguments = string.Join(" ", parts.Skip(1));
+            string directory = new FileInfo(m_objProcfile.FileName).DirectoryName;
+            string fullPath = this.WhereIs(fileName, directory);
+
+            if (fullPath is null)
+            {
+                TextReceived(this, "Not found: " + fileName);
+                return;
+            }
+
+            fileName = fullPath;
+
+            m_objProcess = new Process();
+
+            if (m_envVariables.Count > 0)
+            {
+                foreach (var variable in m_envVariables)
+                {
+                    var key = variable.Key;
+                    var value = variable.Value;
+
+                    var environmentVariables = m_objProcess.StartInfo.EnvironmentVariables;
+                    if (!environmentVariables.ContainsKey(key))
+                    {
+                        environmentVariables.Add(key, value);
+                    } else
+                    {
+                        environmentVariables[key] = value;
+                    }
+                    TextReceived(this, $"{key} = {value}");
+                }
+            }
+
+            m_objProcess.StartInfo.CreateNoWindow = true;
+            m_objProcess.StartInfo.UseShellExecute = false;
+            m_objProcess.StartInfo.RedirectStandardOutput = true;
+            m_objProcess.StartInfo.RedirectStandardError = true;
+
+            //m_objProcess.StartInfo.FileName = "cmd.exe";
+            //m_objProcess.StartInfo.Arguments = "/interactive /c " + m_strCommand;
+            m_objProcess.StartInfo.FileName = fileName;
+            m_objProcess.StartInfo.Arguments = arguments;
+            m_objProcess.StartInfo.WorkingDirectory = directory;
+
+            m_objProcess.EnableRaisingEvents = true;
+            m_objProcess.OutputDataReceived += DataReceived;
+            m_objProcess.ErrorDataReceived += DataReceived;
+            m_objProcess.Exited += ProcessExited;
+
+            TextReceived(this, "Starting: " + m_strCommand);
+
+            m_objProcess.Start();
+            //Process process = m_objProcess.Parent();
+            //TextReceived(this, "Parent: " + process.ProcessName);
+
+            m_objProcess.BeginOutputReadLine();
+            m_objProcess.BeginErrorReadLine();
+            this.Active = !m_objProcess.HasExited;
+
+        }
+
+        public void Stop()
+        {
+
+            if(m_objProcess is null)
+            {
+                return;
+            }
+
+            m_objProcess.KillAllSubProcesses();
+            if (!m_objProcess.HasExited)
+            {
+                m_objProcfile.Info(this, "Stopping process");
+                m_objProcess.Kill();
+            }
+        }
+
+        public string WhereIs(string command, string directory)
+        {
+
+            if (File.Exists(command))
+            {
+                return command;
+            }
+
+            string paths = directory + ';' + Environment.GetEnvironmentVariable("PATH");
+            string[] folders = paths.Split(';');
+
+            List<string> names = new List<string>();
+            names.Add(command);
+
+            if(!(command.EndsWith(".exe") || command.EndsWith(".bat")))
+            {
+                names.Add($"{command}.bat");
+                names.Add($"{command}.exe");
+            }
+
+            foreach(string name in names)
+            {
+                foreach (string folder in folders)
+                {
+
+                    if (!Directory.Exists(folder))
+                    {
+                        continue;
+                    }
+
+                    string path = Path.Combine(folder, name); ;
+                    if (File.Exists(path))
+                    {
+                        return path;
+                    }
+                }
+            }
+ 
+
+            return null;
+        }
+
+        private void DataReceived(object objSender, DataReceivedEventArgs args)
+        {
+            TextReceived(this, args.Data);
+        }
+
+        private void ProcessExited(object objSender, EventArgs args)
+        {
+            this.Active = false;
+            PortFinderService.ReleasePort(this.m_port);
+            m_objProcfile.Info(this, "Process terminated");
+        }
+    }
+}
---
+++ Foreman/UI/frmMain.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Foreman
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.mnsMain = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openProcfileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dlgOpenProcfile = new System.Windows.Forms.OpenFileDialog();
+            this.toolBar = new System.Windows.Forms.ToolStrip();
+            this.tbiStart = new System.Windows.Forms.ToolStripButton();
+            this.tbiStop = new System.Windows.Forms.ToolStripButton();
+            this.tbiClear = new System.Windows.Forms.ToolStripButton();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabControl = new Foreman.TabControl();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.systrayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.mnsMain.SuspendLayout();
+            this.toolBar.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnsMain
+            // 
+            this.mnsMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.mnsMain.Location = new System.Drawing.Point(0, 0);
+            this.mnsMain.Name = "mnsMain";
+            this.mnsMain.Size = new System.Drawing.Size(806, 24);
+            this.mnsMain.TabIndex = 1;
+            this.mnsMain.Text = "menuStrip1";
+            this.mnsMain.Resize += new System.EventHandler(this.frmMain_Resize);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openProcfileToolStripMenuItem,
+            this.stopStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openProcfileToolStripMenuItem
+            // 
+            this.openProcfileToolStripMenuItem.Name = "openProcfileToolStripMenuItem";
+            this.openProcfileToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.openProcfileToolStripMenuItem.Text = "&Open Procfile...";
+            this.openProcfileToolStripMenuItem.Click += new System.EventHandler(this.openProcfileToolStripMenuItem_Click);
+            // 
+            // stopStripMenuItem
+            // 
+            this.stopStripMenuItem.Name = "stopStripMenuItem";
+            this.stopStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.stopStripMenuItem.Text = "Stop";
+            this.stopStripMenuItem.Click += new System.EventHandler(this.stopStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // dlgOpenProcfile
+            // 
+            this.dlgOpenProcfile.Filter = "Procfiles|Procfile*";
+            // 
+            // toolBar
+            // 
+            this.toolBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tbiStart,
+            this.tbiStop,
+            this.tbiClear});
+            this.toolBar.Location = new System.Drawing.Point(0, 24);
+            this.toolBar.Name = "toolBar";
+            this.toolBar.Size = new System.Drawing.Size(806, 25);
+            this.toolBar.TabIndex = 3;
+            // 
+            // tbiStart
+            // 
+            this.tbiStart.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tbiStart.Image = ((System.Drawing.Image)(resources.GetObject("tbiStart.Image")));
+            this.tbiStart.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tbiStart.Name = "tbiStart";
+            this.tbiStart.Size = new System.Drawing.Size(23, 22);
+            this.tbiStart.Text = "Start";
+            this.tbiStart.Click += new System.EventHandler(this.tbiStart_Click);
+            // 
+            // tbiStop
+            // 
+            this.tbiStop.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tbiStop.Image = ((System.Drawing.Image)(resources.GetObject("tbiStop.Image")));
+            this.tbiStop.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tbiStop.Name = "tbiStop";
+            this.tbiStop.Size = new System.Drawing.Size(23, 22);
+            this.tbiStop.Text = "Stop";
+            this.tbiStop.Click += new System.EventHandler(this.tbiStop_Click);
+            // 
+            // tbiClear
+            // 
+            this.tbiClear.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tbiClear.Image = ((System.Drawing.Image)(resources.GetObject("tbiClear.Image")));
+            this.tbiClear.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tbiClear.Name = "tbiClear";
+            this.tbiClear.Size = new System.Drawing.Size(23, 22);
+            this.tbiClear.Text = "Clear console";
+            this.tbiClear.ToolTipText = "Clear console (Ctrl + L)";
+            this.tbiClear.Click += new System.EventHandler(this.tbiClear_Click);
+            // 
+            // imageList
+            // 
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList.Images.SetKeyName(0, "start.png");
+            this.imageList.Images.SetKeyName(1, "stop.png");
+            this.imageList.Images.SetKeyName(2, "clear.png");
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tabControl);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 49);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(806, 462);
+            this.panel1.TabIndex = 4;
+            // 
+            // tabControl
+            // 
+            this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl.Location = new System.Drawing.Point(0, 0);
+            this.tabControl.Multiline = true;
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(806, 462);
+            this.tabControl.TabIndex = 3;
+
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // systrayIcon
+            // 
+            this.systrayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("systrayIcon.Icon")));
+            this.systrayIcon.Text = "Foreman";
+            this.systrayIcon.Visible = true;
+            this.systrayIcon.DoubleClick += new System.EventHandler(this.systrayIcon_DoubleClick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 511);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.toolBar);
+            this.Controls.Add(this.mnsMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.mnsMain;
+            this.Name = "frmMain";
+            this.Text = "Foreman";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmMain_KeyDown);
+            this.mnsMain.ResumeLayout(false);
+            this.mnsMain.PerformLayout();
+            this.toolBar.ResumeLayout(false);
+            this.toolBar.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnsMain;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openProcfileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog dlgOpenProcfile;
+        private System.Windows.Forms.ToolStripMenuItem stopStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolBar;
+        private System.Windows.Forms.ToolStripButton tbiStart;
+        private System.Windows.Forms.ToolStripButton tbiStop;
+        private System.Windows.Forms.ToolStripButton tbiClear;
+        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.NotifyIcon systrayIcon;
+        private TabControl tabControl;
+    }
+}
+
